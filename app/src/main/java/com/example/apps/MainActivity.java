7ef1b2ce3a77bcf1158aca44ffdfa5873b01dd5f
unknown <Admin@IHR80PBF191.continental.edu.pe>
2019-11-12 22:24:16 -0500

    Segundo Cambio

--- app/src/main/java/com/example/apps/MainActivity.java
+++ app/src/main/java/com/example/apps/MainActivity.java
@@ -2,15 +2,43 @@ package com.example.apps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnProducts;
+    private Button btnAboutUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnProducts = (Button) findViewById(R.id.btnEnter);
+        btnAboutUs = (Button) findViewById(R.id.btnAboutUs);
+
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent sirve para cambiar de panatalla
+             //   Log.i(Settings.INFO, "Products");
+                Intent i = new Intent(MainActivity.this, Productos.class);
+                startActivity(i);
+            }
+        });
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v ) {
+              //  Log.i(Settings.INFO, "About us");
+                Intent i = new Intent(MainActivity.this, Abouts.class);
+                startActivity(i);
+            }
+        });
+
+
     }
     @Override
     protected void onResume () {
